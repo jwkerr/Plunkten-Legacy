3972f8c26981a6c21fead90c7638da523a9e2d6e
Fruitloopins <fruitloopins@earthmc.net>
2023-04-21 11:41:02 +1000

    apostrophe

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger("disnake")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename = "disnake.log", encoding = "utf-8", mode = "w")
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 
 bot = commands.InteractionBot()
